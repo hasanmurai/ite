d85e8b376f50e4423d34c1cdfdcf05c284237940
Hasan Murai <hasanmurai56@gmail.com>
2022-07-06 19:17:25 +0300

    a

--- app/Models/ProductLike.php
+++ app/Models/ProductLike.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductLike extends Model
-{
+{//
     use HasFactory;
 
     protected $table = "product_likes";
